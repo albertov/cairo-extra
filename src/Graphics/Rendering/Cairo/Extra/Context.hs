09a63c57facf867b9176e1970da453d5a07a8a94
Alberto Valverde <alberto@toscat.net>
2018-04-16 11:22:05 +0200

    missing LICENSE and cleanup

--- src/Graphics/Rendering/Cairo/Extra/Context.hs
+++ src/Graphics/Rendering/Cairo/Extra/Context.hs
@@ -9,14 +9,12 @@ import Data.Monoid (mempty, (<>))
 import qualified Language.C.Inline as C
 import           Language.C.Inline.Context
 import qualified Language.C.Types as C
-import Foreign.ForeignPtr
 import Graphics.Rendering.Cairo.Types
 
 cairoCtx :: C.Context
 cairoCtx = C.baseCtx <> ctx'
   where ctx' = mempty {
     ctxTypesTable =
-      [ (C.TypeName "cairo_t", [t| Cairo |])
-      , (C.TypeName "cairo_surface_t", [t| Surface |])
+      [ (C.TypeName "cairo_surface_t", [t| Surface |])
       ]
     }
